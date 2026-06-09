e0b8612f05cacffbfd82cf0f1f6e9db800ce012d
Francis Bond <bond@ieee.org>
2026-06-09 21:39:10 +0200

    Update campaign for Wesnoth 1.18.7

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Cities_of_the_Frontier
 
-This is the codebase for Cities of the Frontier, a campaign in the open-source game of Wesnoth that encourages players to build their own city and then defend it from outlaws and orcs. The code has been optimized for Version 1.16 of Wesnoth.
+This is the codebase for Cities of the Frontier, a campaign in the open-source game of Wesnoth that encourages players to build their own city and then defend it from outlaws and orcs. The code targets Wesnoth 1.18.7.
 
 The code was forked from a copy of the Wild Frontiers campaign being maintained by virtualghetto, which in turn was forked from a much earlier version of the original Cities of the Frontier campaign developed most recently by esci.
 
-I will continue to maintain this version for the next few years to deal with bug reports and complaints about game balance, especially if you can be specific about what is wrong, but I have no plans to update the game for future editions of Wesnoth. If you would like to take over as the maintainer for a future edition, please feel free to do so.
+This release updates the campaign's Lua integration and add-on metadata for the Wesnoth 1.18 series.
--- lua/utils.lua
+++ lua/utils.lua
@@ -8,61 +8,64 @@ http://forums.wesnoth.org/viewtopic.php?t=28306&p=414894
 --]]
 
 -- Save and load the map using a WML variable
-local helper = wesnoth.require "lua/helper.lua"
+local function required_attribute(cfg, name, tag)
+  return cfg[name] or wml.error(("[%s] missing required %s= attribute."):format(tag, name))
+end
 
-function save_map(cfg)
-  local b = cfg.border_size or 1
-  local w, h = wesnoth.get_map_size()
+local function save_map(cfg)
+  local map = wesnoth.current.map
+  local b = tonumber(cfg.border_size) or map.border_size
+  local w, h = map.playable_width, map.playable_height
   local t = {}
   for y = 1 - b, h + b do
     local r = {}
-    for x = 1 - b, w + b do r[x + b] = wesnoth.get_terrain(x, y) end
+    for x = 1 - b, w + b do
+      r[x + b] = map[{x, y}]
+    end
     t[y + b] = table.concat(r, ',')
   end
   local s = table.concat(t, '\n')
-  local v = cfg.variable or helper.wml_error "save_map missing required variable= attribute."
-  wesnoth.set_variable(v, string.format("border_size=%d\nusage=map\n\n%s", b, s))
+  local v = required_attribute(cfg, "variable", "save_map")
+  wml.variables[v] = string.format("border_size=%d\nusage=map\n\n%s", b, s)
 end
 wesnoth.wml_actions["save_map"] = save_map
 
 
-function load_map(cfg)
-  local v = cfg.variable or helper.wml_error "load_map missing required variable= attribute."
-  wesnoth.fire("replace_map", { map = wesnoth.get_variable(v), expand = true, shrink = true })
+local function load_map(cfg)
+  local v = required_attribute(cfg, "variable", "load_map")
+  wml.fire.replace_map {
+    map = wml.variables[v],
+    expand = true,
+    shrink = true
+  }
 end
 wesnoth.wml_actions["load_map"] = load_map
 
 
 
 -- Save and load the shroud using a WML variable
-function store_shroud(args)
-   local team_num = args.side or error("~wml:[store_shroud] expects a side= attribute.",0)
-   local storage = args.variable or error("~wml:[store_shroud] expects a variable= attribute.",0)
-   local team = wesnoth.sides[team_num]
-   local current_shroud = team.__cfg.shroud_data
-   wesnoth.set_variable(storage,current_shroud)
+local function store_shroud(cfg)
+  local side_num = tonumber(required_attribute(cfg, "side", "store_shroud"))
+  local storage = required_attribute(cfg, "variable", "store_shroud")
+  local side = wesnoth.sides[side_num]
+  if not side then
+    wml.error(("[store_shroud] invalid side=%s."):format(cfg.side))
+  end
+  wml.variables[storage] = side.shroud_data
 end
 wesnoth.wml_actions["store_shroud"] = store_shroud
 
 
-function set_shroud(args)
-   local team_num = tonumber(args.side) or helper.wml_error "[store_shroud] expects a side= attribute."
-   local shroud = args.shroud_data or helper.wml_error "[store_shroud] expects a shroud_data= attribute."
-   if string.sub(shroud,1,1) ~= "|" then
-      helper.wml_error "[set_shroud] was passed an invalid shroud string."
-   else
-      local w,h,b = wesnoth.get_map_size()
-      local shroud_x = 1 - b
-      for r in string.gmatch(shroud, "|(%d*)") do
-         local shroud_y = 1 - b
-         for c in string.gmatch(r, "%d") do
-            if c == "1" then
-               wesnoth.fire("remove_shroud", { side=team_num, x=shroud_x, y=shroud_y })
-            end
-            shroud_y = shroud_y + 1
-         end
-         shroud_x = shroud_x + 1
-      end
-   end
+local function set_shroud(cfg)
+  local side_num = tonumber(required_attribute(cfg, "side", "set_shroud"))
+  local shroud = required_attribute(cfg, "shroud_data", "set_shroud")
+  local side = wesnoth.sides[side_num]
+  if not side then
+    wml.error(("[set_shroud] invalid side=%s."):format(cfg.side))
+  end
+  if shroud:sub(1, 1) ~= "|" then
+    wml.error("[set_shroud] was passed an invalid shroud string.")
+  end
+  side.shroud_data = shroud
 end
 wesnoth.wml_actions["set_shroud"] = set_shroud
